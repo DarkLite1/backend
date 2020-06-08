85f20cf45429e07c28c7eeadb59aa2472d5e31a8
DarkLite1 <BrechtGijbels@gmail.com>
2020-06-08 11:47:45 +0200

    separated apolloServer form index file

---
+++ src/apolloServer.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { HelloWorldResolver } from './resolvers/HelloWorldResolver'
+import { MovieResolver } from './resolvers/MovieResolver'
+
+export const getApolloServer = async () => {
+  return new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloWorldResolver, MovieResolver],
+    }),
+    context: ({ req, res }) => ({ req, res }),
+  })
+}
--- src/index.ts
+++ src/index.ts
@@ -1,83 +1,13 @@
 import 'reflect-metadata'
 import express from 'express'
 import { createConnections } from 'typeorm'
-import { ApolloServer } from 'apollo-server-express'
-import { buildSchema } from 'type-graphql'
-import { HelloWorldResolver } from './resolvers/HelloWorldResolver'
-import { MovieResolver } from './resolvers/MovieResolver'
 import { ENVIRONMENT } from './environment'
-
+import { getApolloServer } from './apolloServer'
 ;(async () => {
   const app = express()
 
   await createConnections()
-
-  // try {
-  //   await createConnection({
-  //     name: 'it-portal',
-  //     host: 'GRPSDFRAN0049',
-  //     username: 'itportaladmin',
-  //     password: 'eQnG0xzbfKgPbdkf01xQ',
-  //     type: 'mssql',
-  //     database: 'IT-Portal',
-  //     synchronize: false,
-  //     logging: true,
-  //     entities: ['src/it-portal/entity/**/*.ts'],
-  //     migrations: ['src/it-portal/migration/**/*.ts'],
-  //     subscribers: ['src/it-portal/subscriber/**/*.ts'],
-  //     cli: {
-  //       entitiesDir: 'src/it-portal/entity',
-  //       migrationsDir: 'src/it-portal/migration',
-  //       subscribersDir: 'src/it-portal/subscriber',
-  //     },
-  //   })
-  //   console.log('connectionProd is ok')
-  // } catch (error) {
-  //   console.log('Failed connecting to connectionProd: ', error)
-  // }
-
-  // try {
-  //   await createConnection({
-  //     name: 'it-portal-test',
-  //     host: 'GRPSDFRAN0049',
-  //     username: 'itportaltestadmin',
-  //     password: 'ySH56TFuAADpy7GCMA3L',
-  //     type: 'mssql',
-  //     database: 'IT-Portal-TEST',
-  //     synchronize: true,
-  //     logging: true,
-  //     entities: ['src/entity/**/*.ts'],
-  //     migrations: ['src/migration/**/*.ts'],
-  //     subscribers: ['src/subscriber/**/*.ts'],
-  //     cli: {
-  //       entitiesDir: 'src/entity',
-  //       migrationsDir: 'src/migration',
-  //       subscribersDir: 'src/subscriber',
-  //     },
-  //   })
-  //   console.log('connectionTest is ok')
-  // } catch (error) {
-  //   console.log('Failed connecting to connectionTest: ', error)
-  // }
-
-  // if (ENVIRONMENT.mode === 'production') {
-  // } else {
-  //   // const secondConnection: Connection = await createConnection(
-  //   //   'it-portal-connection-test'
-  //   // )
-  // }
-
-  // await createConnection('it-portalDatabase')
-  // await createConnection('default')
-
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [HelloWorldResolver, MovieResolver],
-    }),
-    context: ({ req, res }) => ({ req, res }),
-  })
-
-  apolloServer.applyMiddleware({ app, cors: false })
+  ;(await getApolloServer()).applyMiddleware({ app, cors: false })
 
   app
     .listen({ port: ENVIRONMENT.port, host: ENVIRONMENT.host }, () => {
