d691318866acebd47ecd33650231da77fd9a6120
DarkLite1 <BrechtGijbels@gmail.com>
2020-06-08 13:40:05 +0200

    added apollos-boost for jest testing

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,6 @@ import { getApolloServer } from './apolloServer'
 ;(async () => {
   const app = express()
 
-  console.log('secret: ', process.env.SECRET);
-  
-
   await createConnections()
   ;(await getApolloServer()).applyMiddleware({ app, cors: false })
 
