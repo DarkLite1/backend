492790c180b95c3f91ee3f3ddded54c84a74582d
DarkLite1 <BrechtGijbels@gmail.com>
2020-09-30 09:26:13 +0200

    added viewer preference type

--- src/it-portal/entity/Viewer.ts
+++ src/it-portal/entity/Viewer.ts
@@ -3,7 +3,6 @@ import { Preference } from '@it-portal/entity/Preference'
 
 @ObjectType()
 export class Viewer {
-  // @Field({ nullable: true })
   @Field(() => Preference, { nullable: true })
   preference?: Preference
 }
