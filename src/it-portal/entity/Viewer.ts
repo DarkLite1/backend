22c044fe1b1e5bb87ff88c85127f7d34ab5dd3b8
DarkLite1 <BrechtGijbels@gmail.com>
2020-09-30 09:23:08 +0200

    added ID field for graphql

--- src/it-portal/entity/Viewer.ts
+++ src/it-portal/entity/Viewer.ts
@@ -3,7 +3,7 @@ import { Preference } from '@it-portal/entity/Preference'
 
 @ObjectType()
 export class Viewer {
-  @Field({ nullable: true })
-  //   @Field(() => Preference, { nullable: true })
-  preference: Preference
+  // @Field({ nullable: true })
+  @Field(() => Preference, { nullable: true })
+  preference?: Preference
 }
