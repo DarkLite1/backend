84f0c2eea5fb519e19d4d2f8d11b7d1f9debfe2b
DarkLite1 <BrechtGijbels@gmail.com>
2020-11-26 15:51:03 +0100

    fixed types for Driver entity

--- src/sap-truck-roster/entity/Driver.ts
+++ src/sap-truck-roster/entity/Driver.ts
@@ -1,49 +1,49 @@
-import { Field, ID, ObjectType } from 'type-graphql'
+import { Field, ID, Int, ObjectType } from 'type-graphql'
 
 @ObjectType()
 export class Driver {
   @Field(() => ID)
   readonly id: string
 
-  @Field()
-  readonly firstName: string
+  @Field({ nullable: true })
+  readonly firstName?: string
 
-  @Field()
-  readonly lastName: string
+  @Field({ nullable: true })
+  readonly lastName?: string
 
-  @Field()
-  readonly email: string
+  @Field({ nullable: true })
+  readonly email?: string
 
-  @Field()
-  readonly telephone: string
+  @Field({ nullable: true })
+  readonly telephone?: string
 
-  @Field()
-  readonly despatchGroup: string
+  @Field({ nullable: true })
+  readonly despatchGroup?: string
 
-  @Field()
-  readonly country: string
+  @Field({ nullable: true })
+  readonly country?: string
 
-  @Field()
-  readonly postCode: string
+  @Field( { nullable: true })
+  readonly postCode?: string
 
-  @Field()
-  readonly city: string
+  @Field({ nullable: true })
+  readonly city?: string
 
-  @Field()
-  readonly streetHouse: string
+  @Field({ nullable: true })
+  readonly streetHouse?: string
 
-  @Field()
-  readonly additionalRefNr: string
+  @Field({ nullable: true })
+  readonly additionalRefNr?: string
 
-  @Field()
-  readonly specialProp: string
+  @Field({ nullable: true })
+  readonly specialProp?: string
 
-  @Field()
-  readonly mainWorkPlant: string
+  @Field({ nullable: true })
+  readonly mainWorkPlant?: string
 
-  @Field()
-  readonly createdOn: string
+  @Field({ nullable: true })
+  readonly createdOn?: string
 
-  @Field()
-  readonly deletionFlag: string
+  @Field({ nullable: true })
+  readonly deletionFlag?: string
 }
