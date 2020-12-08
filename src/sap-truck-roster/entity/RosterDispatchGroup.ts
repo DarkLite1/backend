e850372f9f62fe9692ae8b50377892faa89289e6
DarkLite1 <BrechtGijbels@gmail.com>
2020-12-08 08:18:21 +0100

    removed comments

--- src/sap-truck-roster/entity/RosterDispatchGroup.ts
+++ src/sap-truck-roster/entity/RosterDispatchGroup.ts
@@ -5,11 +5,6 @@ export class RosterDispatchGroup {
   @Field()
   readonly date: string
 
-  // @Field()
-  // date2(@Root() parent: RosterDispatchGroup): Date {
-  //   return new Date(parent.date)
-  // }
-
   @Field(() => [String])
   readonly dispatchGroup: string[]
 }
